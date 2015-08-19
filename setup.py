c3414fc4a58dbadf585b8190325b40f1fb7b5aba
Vladimir Zhukov <zhvv117@gmail.com>
2015-08-19 14:40:58 +0300

    new PyPi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "PyOpenGL_game",
-    version = "1.01",
+    version = "1.02",
     author = "Vladimir Zhukov",
     author_email = "zhvv117@gmail.com",
     description = "Simple OpenGL game",
